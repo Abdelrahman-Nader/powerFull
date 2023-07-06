d07a908c430ac8ba8670d2bc0008d8e05fadbb10
“abdo” <abualiya.2202@gmail.com>
2023-07-06 06:21:26 +0300

    issu with ngbModule

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { HttpClientModule } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
@@ -21,7 +22,8 @@ import { RouterModule } from '@angular/router';
     RouterModule,
     HttpClientModule,
     // NavModule,
-    LayoutModule
+    LayoutModule,
+    NgbModule
   ],
   providers: [],
   bootstrap: [AppComponent]
